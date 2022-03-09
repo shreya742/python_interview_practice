84497f8accca411ca264bd3d4c7f5a8a49f8dc51
Sudireddy Sai Shreya <94278880+shreya742@users.noreply.github.com>
2022-03-09 19:52:34 +0530

    Add files via upload

---
+++ ascii_char.py
@@ -0,0 +1,12 @@
+#PROGRAM TO FIND ASCII VALUE OF A CHARACTER.
+
+ch = input("Enter a character: ")
+print("ASCII value of",  ch ,"is ", ord(ch))
+#used to print the ascii value of character.
+
+'''
+OUTPUT:
+-------
+Enter a character: S
+ASCII value of S is  83
+'''
