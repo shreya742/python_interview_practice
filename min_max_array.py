a4c5e3a277f62c5a9ce81253a01660bc0ec84d2b
Sudireddy Sai Shreya <94278880+shreya742@users.noreply.github.com>
2022-05-29 13:35:32 +0530

    Add files via upload

---
+++ min_max_array.py
@@ -0,0 +1,79 @@
+'''
+Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
+
+ 
+
+Example 1:
+
+Input:
+N = 6
+A[] = {3, 2, 1, 56, 10000, 167}
+Output:
+min = 1, max =  10000
+ 
+
+Example 2:
+
+Input:
+N = 5
+A[]  = {1, 345, 234, 21, 56789}
+Output:
+min = 1, max = 56789
+ 
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function getMinMax() which takes the array A[] and its size N as inputs and returns the minimum and maximum element of the array.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 105
+1 <= Ai <=1012
+
+
+'''
+
+#User function Template for python3
+
+def getMinMax( a, n):
+    i = 0
+    min = max = a[i]
+    while(i <= n-1):
+        if max < a[i]:
+            max = a[i]
+        if min > a[i]:
+            min = a[i]
+        i = i + 1
+    return min, max
+    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        
+        product = getMinMax(a, n)
+        print(product[0], end=" ")
+        print(product[1])
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# } Driver Code Ends
